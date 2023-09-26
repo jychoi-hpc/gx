9a38a16fe16a232f574130dc35b35202daa9c38d
Matt Landreman <mattland@umd.edu>
2023-09-26 15:26:44 -0400

    Sign flip for fprim and tprim for m=1,2,3

--- src/device_funcs.cu
+++ src/device_funcs.cu
@@ -2746,7 +2746,7 @@ __global__ void rhs_linear(const cuComplex* __restrict__ g,
 	  cuComplex upar_bar_i = (nspecies>1 && as_i>0) ? kperp2_*bmag_*bmag_*apar_/as_i - nz_*vt_*upar_bar_/(nzvt_i) : make_cuComplex(0.,0.);
 
 	  rhs[globalIdx] = rhs[globalIdx] 
-           - vt_ * iky_ * apar_ * (
+           + vt_ * iky_ * apar_ * (
               Jflr(l-1,b_s)*l*tprim_
 	    + Jflr(l,  b_s)*(fprim_ + (2*l+1)*tprim_)
 	    + Jflr(l+1,b_s)*(l+1)*tprim_ 
@@ -2755,14 +2755,14 @@ __global__ void rhs_linear(const cuComplex* __restrict__ g,
            + phi_ * Jflr(l,b_s) * uprim_ * iky_ / vt_; // need to set uprim_ more carefully; this is a placeholder
 	}
 	if (m==2) {
-	  rhs[globalIdx] = rhs[globalIdx] + iky_*phi_*Jflr(l,b_s)/sqrtf(2.)*tprim_ 
-	     + iky_/zt_*bpar_*JflrB(l, b_s)/sqrtf(2.)*tprim_ 
+	  rhs[globalIdx] = rhs[globalIdx] - iky_*phi_*Jflr(l,b_s)/sqrtf(2.)*tprim_ 
+	     - iky_/zt_*bpar_*JflrB(l, b_s)/sqrtf(2.)*tprim_ 
 	     + ( nu_ + nuei_ ) * sqrtf(2.) * Jflr(l,b_s) * t_bar_;
 	}  
 
 	if (m==3) {
 	  rhs[globalIdx] = rhs[globalIdx] 
-           - vt_ * iky_ * apar_ * sqrtf(3./2.) * tprim_ * Jflr(l,b_s);
+           + vt_ * iky_ * apar_ * sqrtf(3./2.) * tprim_ * Jflr(l,b_s);
         }
       } // l loop
     } // m loop
