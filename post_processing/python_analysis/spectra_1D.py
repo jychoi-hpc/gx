dc73080f052b180fa09f6281fee27dd11fac156a
physycola <toby.adkins1@gmail.com>
2024-03-21 12:38:29 +1100

    merged scripts for plotting 1D spectra; added separate plotting of modes.

--- post_processing/python_analysis/spectra_1D.py
+++ post_processing/python_analysis/spectra_1D.py
@@ -1,8 +1,186 @@
+import numpy as np
 import matplotlib
+import matplotlib.pyplot as plt
+from itertools import cycle, islice
 import sys
 from load_files import load_files
 from extract_species import extract_species
-from plot_spectra_1D import plot_spectra_1D
+
+
+def plot_spectra_1D(simulations, spectrum, spectrum_label, spectrum_range, spectrum_range_label, spectrum_xscale, spectrum_yscale, average_fraction=0.5, absolute_value=False, time=False, modes=False):
+
+    plt.close("all")
+
+    # print("")
+    # print("Taking reference species from first simulation provided.")
+    print('─' * 100)
+
+    ref_species_list = []
+    electromagnetic_spectra_list = ['Phi2kyt', 'Phi2kxt', 'Phi2kzt', 'Phi2zt'] # Electromagnetic spectra must be handled separately as they do not have a species index
+
+    # Initialising plotting
+    fig, (ax) = plt.subplots(1, 1)
+    fig.canvas.manager.set_window_title("One-dimensional spectra (averaged)")
+    plot_color = plt.cm.rainbow(np.linspace(0, 1, len(simulations.keys())))
+
+    spectrum_count = 0
+
+    # Iterating over similation files 
+    for simulation_index, simulation_key in enumerate(simulations.keys()):
+
+        # Selecting simulation
+        simulation = simulations[simulation_key]
+
+        # Check whether simulation has same reference species
+        species_list, ref_species = extract_species(simulation)
+        ref_species_list.append(ref_species)
+
+        if not (ref_species == ref_species_list[0]):
+            print("{:} -- WARNING: Inconsistent reference species.".format(simulation_key.name))
+            continue
+
+        # Extracting spectra and averaging
+        try:
+            spectrum_plot = simulation['Spectra'][str(spectrum)]
+            plot_range    = simulation['Dimensions'][str(spectrum_range)]
+            t_range       = simulation['Dimensions']['time']
+            spectrum_count += 1
+            
+        except:
+            print("%s -- WARNING: '%s' not found." %(simulation_key.name, spectrum))
+            continue
+
+        it_start          = int(len(t_range) * average_fraction)
+        spectrum_time_avg = np.average(spectrum_plot[it_start:], axis = 0)
+
+        # Plotting
+        if ref_species == "ions":
+            linestyles_dict = {"ions": "solid", "electrons": "dashed"} | dict(zip(species_list[2:], list(islice(cycle(["dotted", "dashdot"]), len(species_list[2:])))))
+        else:
+            linestyles_dict = {"ions": "dashed", "electrons": "solid"} | dict(zip(species_list[2:], list(islice(cycle(["dotted", "dashdot"]), len(species_list[2:])))))
+
+        # Plotting time-averaged spectra
+        if spectrum in electromagnetic_spectra_list:
+
+            print(r"%s -- maximum at %s = %.4g" % (simulation_key.name, spectrum_range, plot_range[np.argmax(spectrum_time_avg[1:])]))
+
+            plot_label = simulation_key.name
+
+            ax.plot(plot_range, np.abs(spectrum_time_avg), label=plot_label, linewidth=2, linestyle='solid', marker='o', color=plot_color[simulation_index])
+        else:
+            for species_index, species in enumerate(species_list):
+
+                print(r"%s -- %s: maximum at %s = %.4g" % (simulation_key.name, species_list[species_index], spectrum_range, plot_range[np.argmax(spectrum_time_avg[species_index, :])]))
+
+                if species_index == 0:
+                    plot_label = simulation_key.name
+                else:
+                    plot_label = None
+
+                if absolute_value:
+                    ax.plot(plot_range, np.abs(spectrum_time_avg[species_index, :]), label=plot_label, linewidth=2, linestyle=linestyles_dict[species], marker='o', color=plot_color[simulation_index])
+                else:
+                    ax.plot(plot_range, spectrum_time_avg[species_index, :], label=plot_label, linewidth=2, linestyle=linestyles_dict[species], marker='o', color=plot_color[simulation_index])
+
+        # Plotting the spectrum as a function of time
+        if time:
+
+            fig, (ax_time) = plt.subplots(1, 1)
+            fig.canvas.manager.set_window_title("One-dimensional spectra (%s)" % simulation_key.name)
+
+            norm     = matplotlib.colors.Normalize(vmin=0, vmax=t_range.max())
+            colorbar = plt.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap = "rainbow"), ax=ax_time)
+            colorbar.set_label(r"$t(\sqrt{2}a/v_{\mathrm{th}%s})$" % ref_species[0], labelpad = 15)
+
+            interval_scale = 1
+            it_count       = len(t_range)//interval_scale
+            plot_color     = plt.cm.rainbow(np.linspace(0, 1, num=it_count, endpoint=False))
+            
+            # Electromagnetic spectra must be handled separately as they do not have a species index
+            if spectrum in electromagnetic_spectra_list:
+                for it_index, it in enumerate(np.linspace(0, len(t_range), num=it_count, dtype=int, endpoint=False)):
+                    if absolute_value:
+                        ax_time.plot(plot_range, np.abs(spectrum_plot[it, :]), linewidth=2, linestyle='solid', marker='o', color=plot_color[it_index])
+                    else:
+                        ax_time.plot(plot_range, spectrum_plot[it, :], linewidth=2, linestyle='solid', marker='o', color=plot_color[it_index])
+            else:
+                for species_index, species in enumerate(species_list):
+                    for it_index, it in enumerate(np.linspace(0, len(t_range), num=it_count, dtype=int, endpoint=False)):
+                        if absolute_value:
+                            ax_time.plot(plot_range, np.abs(spectrum_plot[it, species_index, :]), linewidth=2, linestyle=linestyles_dict[species], marker = 'o', color=plot_color[it_index])
+                        else:
+                            ax_time.plot(plot_range, spectrum_plot[it, species_index, :], linewidth=2, linestyle=linestyles_dict[species], marker = 'o', color=plot_color[it_index])
+            
+            ax_time.set_xscale(spectrum_xscale)
+            ax_time.set_xlabel(spectrum_range_label)
+            ax_time.set_yscale(spectrum_yscale)
+            ax_time.set_ylabel(spectrum_label)
+
+        # Plotting the time-evolution of the individual modes
+        if modes:
+
+            fig, (ax_modes) = plt.subplots(1, 1)
+            fig.canvas.manager.set_window_title("One-dimensional spectra (%s)" % simulation_key.name)
+
+            k_start = plot_range[1]
+            k_stop  = plot_range[-1]
+            ik_start = np.absolute(plot_range - k_start).argmin()
+            ik_stop  = np.absolute(plot_range - k_stop).argmin()
+
+            norm     = matplotlib.colors.Normalize(vmin=plot_range[1], vmax=plot_range[-1]) # Colours are normalised to the full range of possible modes, and we select a subset.
+            colorbar = plt.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap="rainbow"), ax=ax_modes)
+            colorbar.set_label(spectrum_range_label, labelpad = 15)
+
+            interval_scale = 1
+            ik_count       = len(plot_range[ik_start:ik_stop])//interval_scale
+            plot_color     = plt.cm.rainbow(np.linspace(0, 1, num=len(plot_range), endpoint=False))
+
+            # Electromagnetic spectra must be handled separately as they do not have a species index
+            if spectrum in electromagnetic_spectra_list:
+                for ik_index, ik in enumerate(np.linspace(ik_start, (ik_stop), num=ik_count, dtype=int, endpoint=False)):
+                    if absolute_value:
+                        ax_modes.plot(t_range, np.abs(spectrum_plot[:, ik]), linewidth=2, linestyle='solid', marker='', color=plot_color[ik])
+                    else:
+                        ax_modes.plot(t_range, spectrum_plot[:, ik], linewidth=2, linestyle='solid', marker='', color=plot_color[ik])
+            else:
+                for species_index, species in enumerate(species_list):
+                    for ik_index, ik in enumerate(np.linspace(ik_start, (ik_stop), num=ik_count, dtype=int, endpoint=False)):
+                        if absolute_value:
+                            ax_modes.plot(t_range, np.abs(spectrum_plot[:, species_index, ik]), linewidth=2, linestyle=linestyles_dict[species], marker='', color=plot_color[ik])
+                        else:
+                            ax_modes.plot(t_range, spectrum_plot[:, species_index, ik], linewidth=2, linestyle=linestyles_dict[species], marker='', color=plot_color[ik])
+            
+            ax_modes.set_xscale("linear")
+            ax_modes.set_xlabel(r"$t(\sqrt{2}a/v_{\mathrm{th}%s})$" % ref_species[0])
+            ax_modes.set_yscale(spectrum_yscale)
+            ax_modes.set_ylabel(spectrum_label)
+            
+
+    # Checking if there are any spectra to plot
+    if spectrum_count == 0:
+        print('─' * 100)
+        print("Spectrum not found in all simulation files. Returning.")
+        print("")
+    else:
+        # Setting plot options
+        ax.set_xscale(spectrum_xscale)
+        ax.set_xlabel(spectrum_range_label)
+        ax.set_yscale(spectrum_yscale)
+        ax.set_ylabel(spectrum_label)
+
+        # ax.legend(labelcolor='linecolor', handlelength = 0, title = "Simulations")
+        ax.legend(handlelength = 0, title = "Simulations")
+
+        if spectrum not in electromagnetic_spectra_list:
+            print("")
+            print("Linestyles:", linestyles_dict)
+        print('─' * 100)
+        print("Completed.")
+        print("")
+
+        plt.show()
+    
+    return
 
 
 if __name__ == "__main__":
@@ -25,10 +203,16 @@ if __name__ == "__main__":
         print("")
         print("Using LaTeX")
 
+    # Setting plotting options
     time = False
     if "time" in filenames:
         filenames.remove("time")
         time = True
+
+    modes = False
+    if "modes" in filenames:
+        filenames.remove("modes")
+        modes = True
 
     # Lists of spectra
     spectra_kx       = ["Wkxst", "Pkxst", "Qkxst", "Gamkxst", "Phi2kxt"]
@@ -103,4 +287,4 @@ if __name__ == "__main__":
                 spectrum_yscale      = "linear"
                 
             plot_spectra_1D(simulations, spectrum=spectra_plot[spectrum_index], spectrum_label=spectra_label_plot[spectrum_index], \
-                                spectrum_range=spectrum_range, spectrum_range_label=spectrum_range_label, spectrum_xscale=spectrum_xscale, spectrum_yscale=spectrum_yscale, average_fraction=0.5, absolute_value=False, time=time)
+                                spectrum_range=spectrum_range, spectrum_range_label=spectrum_range_label, spectrum_xscale=spectrum_xscale, spectrum_yscale=spectrum_yscale, average_fraction=0.5, absolute_value=False, time=time, modes=modes)
