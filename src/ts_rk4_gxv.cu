e34f1a7cf28c369d8eb42881b125abcf6b9afb68
Ian Abel <ian@utumno.org.uk>
2024-04-05 13:15:29 -0400

    merge

--- src/ts_rk4_gxv.cu
+++ src/ts_rk4_gxv.cu
@@ -4,7 +4,7 @@
 
 // ============= RK4 =============
 GXVRK4::GXVRK4(Linear *linear, Nonlinear *nonlinear, Solver *solver,
-			 Parameters *pars, Grids *grids, Forcing *forcing, double dt_in) :
+			 Parameters *pars, Grids *grids, Forcing *forcing, ExB *exb, double dt_in) :
   linear_(linear), nonlinear_(nonlinear), solver_(solver), grids_(grids), pars_(pars),
   forcing_(forcing), dt_max(dt_in), dt_(dt_in),
   ctx(), GStar(pars, grids, ctx), GRhs(pars, grids, ctx), G_q1(pars, grids, ctx), G_q2(pars, grids, ctx)
