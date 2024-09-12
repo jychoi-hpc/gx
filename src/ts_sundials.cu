9fcfc98e9956705154843f1fa487c3880312d9d6
Ian Abel <iga@physics.org>
2024-09-12 14:16:26 -0400

    test relaxing rtol near grid boundaries

--- src/ts_sundials.cu
+++ src/ts_sundials.cu
@@ -184,10 +184,10 @@ int SundialsStepper::ErrorWeights( GXVector *g, GXVector *weights )
     for( int i = 0; i < g->nSpecies(); ++i )
     {
         MomentsG const & m = *((*g)[ i ]);
-        if( Wg_data.size() == grids_->Nspecies )
-          setWeightsKernel<<< m.dG_all, m.dB_all >>> ( weights->gData( i ), m, abstol, reltol, Wg_data[ i ] );
-        else
-          setWeightsKernel<<< m.dG_all, m.dB_all >>> ( weights->gData( i ), m, abstol, reltol, 0.0 );
+//        if( Wg_data.size() == grids_->Nspecies )
+//          setWeightsKernel<<< m.dG_all, m.dB_all >>> ( weights->gData( i ), m, abstol, reltol, Wg_data[ i ] );
+//        else
+        setWeightsKernel<<< m.dG_all, m.dB_all >>> ( weights->gData( i ), m, abstol, reltol, 0.0 );
 
         checkCuda( cudaGetLastError() );
     }
