ca2851277d4645f2ef0f1fc7fe55e4882853bc43
Ian Abel <iga@physics.org>
2024-09-12 16:14:12 -0400

    spurious extra agrument

--- include/gxvector.h
+++ include/gxvector.h
@@ -76,7 +76,7 @@ class GXVector {
 
 		float MaxNorm() const;
 		float WrmsNorm( GXVector const & ) const;
-		float Norm( GXVector const & ) const;
+		float Norm() const;
 
 
 		float MinReal() const;
