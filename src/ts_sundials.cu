43f11023fcad9f9d343fd789487b2609812e5a06
Ian Abel <iga@physics.org>
2024-09-19 11:40:24 -0400

    incorrect C++ usage

--- src/ts_sundials.cu
+++ src/ts_sundials.cu
@@ -219,12 +219,12 @@ double SundialsStepper::get_dt()
 
 
 // Fixed implementations of non-adaptive RK4 / SSPX2 / SSPX3 / K10 to allow for seamless transition
-sunrealtype SunRK4Stepper::c{0.0,0.5,0.5,1.0};
-sunrealtype SunRK4Stepper::b{1.0/6.0,1.0/3.0,1.0/3.0,1.0/6.0};
-sunrealtype SunRK4Stepper::a{0.0,0.0,0.0,0.0,
-                             0.5,0.0,0.0,0.0,
-                             0.0,0.5,0.0,0.0,
-                             0.0,0.0,1.0,0.0};
+sunrealtype SunRK4Stepper::c[4] = {0.0,0.5,0.5,1.0};
+sunrealtype SunRK4Stepper::b[4] = {1.0/6.0,1.0/3.0,1.0/3.0,1.0/6.0};
+sunrealtype SunRK4Stepper::a[16] = {0.0,0.0,0.0,0.0,
+                                    0.5,0.0,0.0,0.0,
+                                    0.0,0.5,0.0,0.0,
+                                    0.0,0.0,1.0,0.0};
 
 SunRK4Stepper::SunRK4Stepper(Linear *linear, Nonlinear *nonlinear, Solver *solver, Parameters *pars, Grids *grids, Forcing *forcing, ExB *exb, double dt_in, MomentsG** G0, double t0 ) 
     : SundialsStepper( linear, nonlinear, solver, pars, grids, forcing, exb, dt_in, G0, t0 ), rk4table( nullptr )
