dd6155646fc2f3d9302bc2904694a95a2f247708
Ian Abel <iga@physics.org>
2024-11-20 15:18:49 -0500

    enable tolerancce relative to max element

--- src/ts_sundials.cu
+++ src/ts_sundials.cu
@@ -192,10 +192,8 @@ int SundialsStepper::ErrorWeights( GXVector *g, GXVector *weights )
     for( size_t i = 0; i < g->nSpecies(); ++i )
     {
         MomentsG const & m = *((*g)[ i ]);
-//        if( Wg_data.size() == grids_->Nspecies )
-//          setWeightsKernel<<< m.dG_all, m.dB_all >>> ( weights->gData( i ), m, abstol, reltol, Wg_data[ i ] );
-//        else
-        setWeightsKernel<<< m.dG_all, m.dB_all >>> ( weights->gData( i ), m, abstol, reltol, wg_tol, 0.0 );
+        float max_abs_g = g->MaxNorm(); // obtain Max |g| so we can ignore tolerances on modes with |g_i| < 1e-3 * Max(|g|)
+        setWeightsKernel<<< m.dG_all, m.dB_all >>> ( weights->gData( i ), m, abstol, reltol, wg_tol, max_abs_g );
 
         checkCuda( cudaGetLastError() );
     }
